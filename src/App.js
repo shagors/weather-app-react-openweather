d164afd6ec07e2ca8d64ded854491775f45a3c44
shagors <shajjadshagor@gmail.com>
2022-08-15 15:37:16 +0600

    background change and color set and font family change

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import Search from './components/search/Search';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="container">
+      <h1>Weather App</h1>
+      <Search></Search>
     </div>
   );
 }
