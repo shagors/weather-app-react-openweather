a715dbb60c5b717243190ae7c35c1bc5d99256e0
shagors <shajjadshagor@gmail.com>
2022-08-15 16:58:18 +0600

    search by country name

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
+import CurrentWeather from './components/currentWeather/CurrentWeather';
 import Search from './components/search/Search';
 
 function App() {
+
+  const handleOnSearchChange = (searchData) => {
+    console.log(searchData);
+    
+  }
+
   return (
     <div className="container">
-      <h1>Weather App</h1>
-      <Search></Search>
+      <Search onSearchChange={handleOnSearchChange}></Search>
+      <CurrentWeather></CurrentWeather>
     </div>
   );
 }
---
+++ src/api.js
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'e70ef38e88mshbc92fbc9f0b9872p1def33jsnf9c87b7636a5',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
