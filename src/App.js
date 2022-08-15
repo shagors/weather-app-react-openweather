96a89bc9fb31daccc810cd6a571479cc5037c208
shagors <shajjadshagor@gmail.com>
2022-08-15 23:40:14 +0600

    card design complete and all dummy data checked and api call and set api key for check

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { WEATHER_API_KEY, WEATHER_API_URL } from './api';
 import './App.css';
 import CurrentWeather from './components/currentWeather/CurrentWeather';
 import Search from './components/search/Search';
@@ -5,8 +6,17 @@ import Search from './components/search/Search';
 function App() {
 
   const handleOnSearchChange = (searchData) => {
-    console.log(searchData);
+    const [lat, lon] = searchData.value.split(" ");
+
+    const currentWeatherFetch = fetch(
+      `${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`
+    );
+    const forcastFetch = fetch(
+      `${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`
+    );
+
     
+
   }
 
   return (
