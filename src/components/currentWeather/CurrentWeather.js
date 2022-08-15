d1c98c02abc34e86071315df7dad548aaecd2321
shagors <shajjadshagor@gmail.com>
2022-08-15 17:27:56 +0600

    design card and set dummy data for designing

--- src/components/currentWeather/CurrentWeather.js
+++ src/components/currentWeather/CurrentWeather.js
@@ -4,10 +4,24 @@ const CurrentWeather = () => {
     return (
         <div className='weather'>
             <div className='top'>
-                <p className='city'>Belgrade</p>
-                <p className='weather-description'>Sunny</p>
+                <div>
+                    <p className='city'>Belgrade</p>
+                    <p className='weather-description'>Sunny</p>
+                </div>
+                <img alt='weather' className='weather-icon' src='icons/01d.png' />
             </div>
-            <img alt='weather' className='weather-icon' src='' />
+            <div className="bottom">
+                <p className='temperature'>18°C</p>
+                <div className='details'>
+                    <div className='parameter-row'>
+                        <span className='parameter-label'>Details</span>
+                    </div>
+                    <div className='parameter-row'>
+                        <span className='parameter-label'>Feels like</span>
+                        <span className='parameter-value'>28°C</span>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
